07cabdee1286534a6f20951e8b6786c7bee39f50
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2013-12-07 23:43:40 -0500

    removed jinja2htmlcompress in favor of spaceless extension

--- fufufuu/core/templates.py
+++ fufufuu/core/templates.py
@@ -1,8 +1,42 @@
+import re
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext, ungettext
+from jinja2 import nodes
 from jinja2.environment import Environment
+from jinja2.ext import Extension
 from jinja2.loaders import FileSystemLoader
 from fufufuu.settings import TEMPLATE_DIRS, DEBUG
+
+#-------------------------------------------------------------------------------
+
+
+class Spaceless(Extension):
+    """
+    Emulates the django spaceless template tag.
+    """
+
+    tags = set(['spaceless'])
+
+    def parse(self, parser):
+        """
+        Parses the statements and calls back to strip spaces.
+        """
+        lineno = parser.stream.__next__().lineno
+        body = parser.parse_statements(['name:endspaceless'], drop_needle=True)
+        return nodes.CallBlock( self.call_method('_render_spaceless'), [], [], body).set_lineno(lineno)
+
+    def _render_spaceless(self, caller=None):
+        """
+        Strip the spaces between tags using the regular expression
+        from django. Stolen from `django.util.html` Returns the given HTML
+        with spaces between tags removed.
+        """
+        if not caller:
+            return ''
+        return re.sub(r'>\s+<', '><', str(caller().strip()))
+
+
+#-------------------------------------------------------------------------------
 
 
 TEMPLATE_ENV = Environment(
@@ -11,7 +45,7 @@ TEMPLATE_ENV = Environment(
     extensions=[
         'jinja2.ext.i18n',
         'jinja2.ext.with_',
-        'fufufuu.core.jinja2htmlcompress.HTMLCompress',
+        'fufufuu.core.templates.Spaceless',
     ],
 )
 TEMPLATE_ENV.install_gettext_callables(ugettext, ungettext)
