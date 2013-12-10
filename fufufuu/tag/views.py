3617db32cd0718a3a396dae27d781a4385c766c5
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2013-12-09 21:49:06 -0500

    added tag-list-grid view

--- fufufuu/tag/views.py
+++ fufufuu/tag/views.py
@@ -1,6 +1,6 @@
-from django.http.response import Http404
 from django.utils.translation import get_language
 from fufufuu.core.languages import Language
+from fufufuu.core.utils import paginate
 from fufufuu.core.views import TemplateView
 from fufufuu.tag.enums import TagType
 from fufufuu.tag.models import TagData
@@ -13,7 +13,12 @@ class TagListGridView(TemplateView):
     template_name = 'tag/tag-list-grid.html'
 
     def get(self, request):
-        return self.render_to_response({})
+        tag_list = TagData.objects.filter(tag__tag_type=self.tag_type, language=get_language())
+        tag_list = paginate(tag_list, self.page_size, request.GET.get('p'))
+        return self.render_to_response({
+            'tag_list': tag_list,
+            'title': TagType.plural[self.tag_type],
+        })
 
 
 class TagListView(TemplateView):
@@ -22,27 +27,15 @@ class TagListView(TemplateView):
     template_name = 'tag/tag-list.html'
 
     def get(self, request):
-        return self.render_to_response({})
+        tag_list = TagData.objects.filter(tag__tag_type=self.tag_type, language=get_language())
 
-    # def get(self, request, tag_type):
-    #     # Notes: this view should paginate for collections and tanks listing
-    #     # possibly to 120 per page?
-    #
-    #     tag_type = TagType.plural_reverse_map.get(tag_type)
-    #     if tag_type is None:
-    #         raise Http404
-    #
-    #     self.set_template_name(tag_type)
-    #
-    #     tag_list = TagData.objects.filter(tag__tag_type=tag_type, language=get_language())
-    #
-    #     if get_language() != Language.ENGLISH:
-    #         tag_list_missing = TagData.objects.filter(tag__tag_type=TagType.TANK, language=Language.ENGLISH).exclude(tag_id__in=tag_list.values_list('tag_id', flat=True))
-    #     else:
-    #         tag_list_missing = []
-    #
-    #     return self.render_to_response({
-    #         'tag_list': tag_list,
-    #         'tag_list_missing': tag_list_missing,
-    #         'title': TagType.plural[tag_type],
-    #     })
+        if get_language() != Language.ENGLISH:
+            tag_list_missing = TagData.objects.filter(tag__tag_type=self.tag_type, language=Language.ENGLISH).exclude(tag_id__in=tag_list.values_list('tag_id', flat=True))
+        else:
+            tag_list_missing = []
+
+        return self.render_to_response({
+            'tag_list': tag_list,
+            'tag_list_missing': tag_list_missing,
+            'title': TagType.plural[self.tag_type],
+        })
