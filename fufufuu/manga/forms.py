58605a85df2bfb8d54d4c62cc5a9199479cf0626
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2013-12-23 22:08:34 -0500

    added basic processing to manga form

--- fufufuu/manga/forms.py
+++ fufufuu/manga/forms.py
@@ -4,6 +4,7 @@ from fufufuu.core.forms import BlankLabelSuffixMixin
 from fufufuu.core.languages import Language
 from fufufuu.manga.enums import MangaCategory
 from fufufuu.manga.models import Manga
+from fufufuu.tag.enums import TagType
 
 
 class MangaEditForm(BlankLabelSuffixMixin, forms.ModelForm):
@@ -59,47 +60,17 @@ class MangaEditForm(BlankLabelSuffixMixin, forms.ModelForm):
         })
     )
 
-    authors = forms.CharField(
-        required=False,
-        label=_('Authors'),
-    )
-    circles = forms.CharField(
-        required=False,
-        label=_('Circles'),
-    )
-    content = forms.CharField(
-        required=False,
-        label=_('Content'),
-    )
-    events = forms.CharField(
-        required=False,
-        label=_('Events'),
-    )
-    magazines = forms.CharField(
-        required=False,
-        label=_('Magazines'),
-    )
-    parodies = forms.CharField(
-        required=False,
-        label=_('Parodies'),
-    )
-    scanlators = forms.CharField(
-        required=False,
-        label=_('Scanlators'),
-    )
+    authors = forms.CharField(required=False, label=_('Authors'))
+    circles = forms.CharField(required=False, label=_('Circles'))
+    content = forms.CharField(required=False, label=_('Content'))
+    events = forms.CharField(required=False, label=_('Events'))
+    magazines = forms.CharField(required=False, label=_('Magazines'))
+    parodies = forms.CharField(required=False, label=_('Parodies'))
+    scanlators = forms.CharField(required=False, label=_('Scanlators'))
 
-    category = forms.ChoiceField(
-        label=_('Category'),
-        choices=MangaCategory.choices
-    )
-    language = forms.ChoiceField(
-        label=_('Language'),
-        choices=Language.choices
-    )
-    uncensored = forms.BooleanField(
-        required=False,
-        label=_('Uncensored'),
-    )
+    category = forms.ChoiceField(label=_('Category'), choices=MangaCategory.choices)
+    language = forms.ChoiceField(label=_('Language'), choices=Language.choices)
+    uncensored = forms.BooleanField(required=False, label=_('Uncensored'))
 
     class Meta:
         model = Manga
@@ -109,7 +80,31 @@ class MangaEditForm(BlankLabelSuffixMixin, forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.request = request
 
+    def clean_tags(self, tag_type, field_name):
+        tags = self.cleaned_data.get(field_name)
+        for name in tags.split(','):
+            name = name.strip()[:50]
+            if name: self.tags.append((tag_type, name))
+        return tags
+
+    def clean_authors(self):    return self.clean_tags(TagType.AUTHOR, 'authors')
+    def clean_circles(self):    return self.clean_tags(TagType.AUTHOR, 'circles')
+    def clean_content(self):    return self.clean_tags(TagType.AUTHOR, 'content')
+    def clean_events(self):     return self.clean_tags(TagType.AUTHOR, 'events')
+    def clean_magazines(self):  return self.clean_tags(TagType.AUTHOR, 'magazines')
+    def clean_parodies(self):   return self.clean_tags(TagType.AUTHOR, 'parodies')
+    def clean_scanlators(self): return self.clean_tags(TagType.AUTHOR, 'scanlators')
+
+    def clean(self):
+        cd = self.cleaned_data
+        # handle tank and collection
+        return cd
+
     def save(self):
         manga = super().save(commit=False)
         manga.save(updated_by=self.request.user)
+
+        # update tank and collection
+        # process m2m information
+
         return manga
