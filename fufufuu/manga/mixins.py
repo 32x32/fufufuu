279ee26b2b0a3c3c13398b61e408b7b35c4ac82b
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-01-18 14:56:29 -0500

    added displaying of tags on manga-info page

--- fufufuu/manga/mixins.py
+++ fufufuu/manga/mixins.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from fufufuu.core.languages import Language
 from fufufuu.manga.enums import MangaStatus, MangaCategory
 
@@ -12,3 +13,13 @@ class MangaMixin:
 
     @property
     def language_display(self): return Language.choices_dict[self.language]
+
+    @property
+    def tag_dictionary(self):
+        if hasattr(self, '_tag_dictionary'):
+            return self._tag_dictionary
+
+        self._tag_dictionary = defaultdict(list)
+        for tag in self.tags.all():
+            self._tag_dictionary[tag.tag_type].append(tag)
+        return self._tag_dictionary
