f93864f5fcbca97ce0a2564fefa549b61434601c
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-03-09 17:50:20 -0400

    FU-135 - Implemented user upload view

--- fufufuu/user/views.py
+++ fufufuu/user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404
 from fufufuu.account.models import User
+from fufufuu.core.utils import paginate
 from fufufuu.core.views import TemplateView
+from fufufuu.manga.models import Manga
+from fufufuu.manga.views import MangaListMixin
 
 
 class UserViewMixin:
@@ -19,11 +22,20 @@ class UserView(UserViewMixin, TemplateView):
         })
 
 
-class UserUploadsView(UserViewMixin, TemplateView):
+class UserUploadsView(UserViewMixin, MangaListMixin, TemplateView):
 
     template_name = 'user/user-uploads.html'
 
     def get(self, request, username):
+        target_user = self.get_user(username)
+
+        filters = self.get_filters()
+        filters['created_by_id'] = target_user.id
+
+        manga_list = Manga.published.filter(**filters).order_by('-published_on')
+        manga_list = paginate(manga_list, self.page_size, request.GET.get('p'))
+
         return self.render_to_response({
-            'target_user': self.get_user(username),
+            'manga_list': manga_list,
+            'target_user': target_user,
         })
