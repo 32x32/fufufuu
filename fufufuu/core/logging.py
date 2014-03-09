d061fb3dbe4bec694c54c25dbd7eb2b44900d8f0
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-03-09 16:20:45 -0400

    FU-132 - Moved django's cache import to inside email_admin_limit function

--- fufufuu/core/logging.py
+++ fufufuu/core/logging.py
@@ -1,10 +1,9 @@
-from django.core.cache import cache
-
-
 def email_admin_limit(record):
     """
     limit to 1 email sent to admin per minute
     """
+
+    from django.core.cache import cache
 
     CACHE_KEY = 'email_admin_limit'
 
