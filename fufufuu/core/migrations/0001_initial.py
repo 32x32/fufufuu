b5b7084910e588d9f0f9af47d7da5dc220bffd83
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-03-15 19:40:54 -0400

    FU-146 - Moved SiteSetting model into core

--- fufufuu/core/migrations/0001_initial.py
+++ fufufuu/core/migrations/0001_initial.py
@@ -12,24 +12,64 @@ class Migration(SchemaMigration):
         db.create_table('deleted_file', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('path', self.gf('django.db.models.fields.CharField')(max_length=1024)),
-            ('delete_after', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 14, 0, 0))),
-            ('created_on', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now_add=True)),
+            ('delete_after', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 16, 0, 0))),
+            ('created_on', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
         ))
         db.send_create_signal('core', ['DeletedFile'])
+
+        # Adding model 'SiteSetting'
+        db.create_table('site_setting', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('key', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('val', self.gf('django.db.models.fields.CharField')(blank=True, null=True, max_length=255)),
+            ('updated_on', self.gf('django.db.models.fields.DateTimeField')(blank=True, auto_now=True)),
+            ('updated_by', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['account.User'], on_delete=models.SET_NULL, null=True, blank=True)),
+        ))
+        db.send_create_signal('core', ['SiteSetting'])
 
 
     def backwards(self, orm):
         # Deleting model 'DeletedFile'
         db.delete_table('deleted_file')
 
+        # Deleting model 'SiteSetting'
+        db.delete_table('site_setting')
+
 
     models = {
+        'account.user': {
+            'Meta': {'db_table': "'user'", 'object_name': 'User'},
+            'avatar': ('django.db.models.fields.files.FileField', [], {'blank': 'True', 'null': 'True', 'max_length': '255'}),
+            'comment_limit': ('django.db.models.fields.IntegerField', [], {'default': '100'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'blank': 'True', 'max_length': '254'}),
+            'html': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_moderator': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'markdown': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'revision_limit': ('django.db.models.fields.IntegerField', [], {'default': '10'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'upload_limit': ('django.db.models.fields.IntegerField', [], {'default': '10'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
         'core.deletedfile': {
             'Meta': {'db_table': "'deleted_file'", 'object_name': 'DeletedFile'},
-            'created_on': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now_add': 'True'}),
-            'delete_after': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 3, 14, 0, 0)'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'delete_after': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 3, 16, 0, 0)'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'path': ('django.db.models.fields.CharField', [], {'max_length': '1024'})
+        },
+        'core.sitesetting': {
+            'Meta': {'db_table': "'site_setting'", 'object_name': 'SiteSetting'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'updated_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['account.User']", 'on_delete': 'models.SET_NULL', 'null': 'True', 'blank': 'True'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'auto_now': 'True'}),
+            'val': ('django.db.models.fields.CharField', [], {'blank': 'True', 'null': 'True', 'max_length': '255'})
         }
     }
 
--- fufufuu/core/models.py
+++ fufufuu/core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.core.cache import cache
+from django.utils.functional import SimpleLazyObject
+from django.utils.translation import ugettext as _
 from fufufuu.account.models import User
 
 
@@ -44,3 +47,41 @@ class DeletedFile(models.Model):
 
     class Meta:
         db_table = 'deleted_file'
+
+
+
+class SiteSetting(models.Model):
+
+    choices = (
+        ('ANNOUNCEMENT',        _('Announcement')),
+        ('ENABLE_COMMENTS',     _('Enable Comments')),
+        ('ENABLE_UPLOADS',      _('Enable Uploads')),
+    )
+
+    choices_dict = dict(choices)
+
+    key = models.CharField(max_length=255, choices=choices)
+    val = models.CharField(max_length=255, blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = 'site_setting'
+
+    revision_field_list = ['key', 'val']
+
+
+def _get_site_settings():
+    """
+    Try to get site_settings object from cache before going to the database.
+    """
+
+    _site_settings = cache.get('site_settings')
+    if _site_settings: return _site_settings
+
+    _site_settings = dict([(s.key, s.val) for s in SiteSetting.objects.all()])
+    cache.set('site_settings', _site_settings)
+    return _site_settings
+
+
+site_settings = SimpleLazyObject(_get_site_settings)
