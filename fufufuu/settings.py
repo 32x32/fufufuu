ce88ffea9df9c3786dc557ec3abc864c1507d14e
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-03-15 18:40:32 -0400

    FU-143 - Removed Django's admin app (Staff app will be taking its place)

--- fufufuu/settings.py
+++ fufufuu/settings.py
@@ -18,7 +18,6 @@ ALLOWED_HOSTS = []
 INTERNAL_IPS = ['127.0.0.1',]
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
