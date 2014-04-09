742c428710fac2db38646604ab7ed090948b16dc
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-04-08 21:49:15 -0400

    FU-204 - Updated backup.sh, removed v2 cronjobs, and removed logging for uwsgi.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,8 +1,8 @@
-pg_dump -cO -h localhost -U fufufuu_old_user fufufuu_old > dump.sql
+pg_dump -cO -h localhost -U fufufuu_user fufufuu > dump.sql
 rm -f dump.sql.gz
 gzip dump.sql
 FILENAME=$(date +"%Y%m%d")
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
-aws s3 mv dump.sql.gz s3://fufufuu2/db/$YEAR/$MONTH/$FILENAME.sql.gz
-aws s3 sync /var/www/fufufuu2/media/zip s3://fufufuu2/media/zip
+aws s3 mv dump.sql.gz s3://fufufuu/db/$YEAR/$MONTH/$FILENAME.sql.gz
+aws s3 sync /var/www/fufufuu/media/manga-archive s3://fufufuu/media/manga-archive
