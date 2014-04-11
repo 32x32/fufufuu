aa1f9f56023b4111c21e57fb6e0b8c33f127edac
Derek Kwok <derek.kai.chun.kwok@gmail.com>
2014-04-11 01:11:33 -0400

    FU-173 - added path to aws command in backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,5 +4,5 @@ gzip dump.sql
 FILENAME=$(date +"%Y%m%d")
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
-aws s3 mv dump.sql.gz s3://fufufuu/db/$YEAR/$MONTH/$FILENAME.sql.gz
-aws s3 sync --delete /var/www/fufufuu/media/manga-archive s3://fufufuu/media/manga-archive
+/usr/local/bin/aws s3 mv dump.sql.gz s3://fufufuu/db/$YEAR/$MONTH/$FILENAME.sql.gz
+/usr/local/bin/aws s3 sync --delete /var/www/fufufuu/media/manga-archive s3://fufufuu/media/manga-archive
